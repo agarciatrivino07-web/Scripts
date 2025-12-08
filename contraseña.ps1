085a8db6f521bb7428a6722b31b9243d03ee86dd
Axel García <agarciatrivino07@gmail.com>
2025-12-08 22:38:05 +0100

    Script
    Contraseña para restablecer la contraseña de Microsoft.

---
+++ contraseña.ps1
@@ -0,0 +1,12 @@
+﻿
+# Verificar si hay conexión a internet
+$hayConexion = Test-Connection -ComputerName 8.8.8.8 -Count 1 -Quiet
+
+# Si hay conexión, abrir Edge en modo InPrivate
+if ($hayConexion) {
+    Start-Process msedge.exe -ArgumentList "-new-window", "-inprivate", "https://passwordreset.microsoftonline.com/"
+} else {
+    # Mostrar un mensaje de error si no hay conexión
+    [System.Windows.Forms.MessageBox]::Show("No hay conexión a internet. Por favor, verifica tu Wi-Fi.", "Error de Conexión", "OK", "Error")
+}
+
